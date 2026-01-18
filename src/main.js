22eb1ca9ba6921778a09b8acffe89ee41ac116ee
Serhii Muzalevskyi <seregamuza57@gmail.com>
2026-01-18 18:28:40 +0100

    xxxxxxx

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const form = document.querySelector('.form');
+const input = form.elements['search-text'];
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const query = input.value.trim();
+
+  if (!query) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search term!',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (!data.hits.length) {
+        iziToast.warning({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Something went wrong. Try again later!',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      form.reset();
+    });
+});
